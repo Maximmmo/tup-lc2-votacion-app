b8a3d60e2ed687b78229221f0ff08c2c102b2804
JulianPaquez <136456282+JulianPaquez@users.noreply.github.com>
2023-09-18 11:27:17 -0300

    Add files via upload

---
+++ ej1python.py
@@ -0,0 +1,15 @@
+#1-Escribe un programa que pida al usuario ingresar su edad y determine si es mayor de edad (18 años o más) o menor de edad.
+
+
+
+#1
+edad= int(input("Ingrese la edad de la persona\n"))
+
+#print("La edad de la persona es",edad)
+
+if edad >= 18:
+    print("La persona es mayor de edad")
+else:
+    print("La persona es menor de edad")
+
+
